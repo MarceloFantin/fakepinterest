a1e3234da42f70d22ad5e416fa4c55daffba80fe
MarceloFantin <marcelo_fantin@yahoo.com.br>
2026-05-04 21:40:44 -0300

    alterado caminho banco postgresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -10,7 +10,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 app.config["SECRET_KEY"] = "3tvh17ihwt0vtrXnBuDHQw"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
